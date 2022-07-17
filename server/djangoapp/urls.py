c6041c7673138d56c74c714ad1dc080b3cbe78f8
usersd450 <sofiyaansari405@gmail.com>
2022-07-17 11:12:28 +0000

    new

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -2,20 +2,14 @@ from django.urls import path
 from django.conf.urls.static import static
 from django.conf import settings
 from . import views
-
 app_name = 'djangoapp'
 urlpatterns = [
    
     path(route='about/',view=views.about,name='about'),
     path(route='contact/',view=views.contact,name='contact'),
-    # path for contact us view
-
-    # path for registration
-
-    # path for login
-
-    # path for logout
-
+    path(route='logout/',view=views.logout_request,name='logout'),
+    path(route='login/',view=views.login_request,name='login'),
+    path(route='registration/', view=views.registration_request, name='registration'),
     path(route='index/', view=views.get_dealerships, name='index')
 
     # path for dealer reviews view
